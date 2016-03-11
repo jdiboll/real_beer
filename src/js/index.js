6918050f4e5e6b9f6e27bd236f3d862056df0c4e
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-10 19:32:23 -0500

    starting in on the javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 // Javascript Entry Point
+import etsyData from './items';
+import $ from 'jquery';
+
+var products = etsyData.results;
+
+
+function displayItem(item) {
+	var productOne = product.Images[0].url_75x75;
+
+	return ` 
+	<div class="image">
+		<div class="image-photo">
+			<img src="${productOne}" alt"${product.description}">
+		</div>
+	<div class="price">${product.price}</div>;
+
+}
